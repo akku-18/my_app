69d6e65687e8b9edb348d2d7b6deaf958a85c380
akku-18 <akshanshgarg18@gmail.com>
2022-12-29 13:45:34 +0530

    Portfolio creator clone - atri

---
+++ atri_app/app/src/hooks/useStore.js
@@ -0,0 +1,248 @@
+import create from "zustand";
+
+// unsafe merge state
+// and mew properties will added or existing properties will be changed
+// but the type of value of the property must not change
+function mergeState(baseState, newState) {
+  if (
+    typeof newState === "object" &&
+    !Array.isArray(newState) &&
+    newState !== null
+  ) {
+    const keys = Object.keys(newState);
+    keys.forEach((key) => {
+      // create a new key in base if not exists
+      if (!(key in baseState)) {
+        baseState[key] = {};
+      }
+      if (typeof newState[key] === "object" && !Array.isArray(newState[key]))
+        mergeState(baseState[key], newState[key]);
+      else baseState[key] = newState[key];
+    });
+  }
+}
+
+const useStore = create((set) => {
+  return {
+    setPage: (pageName, newState) =>
+      set((state) => {
+        const pageState = JSON.parse(JSON.stringify(state[pageName]));
+        mergeState(pageState, newState);
+        return { [pageName]: pageState };
+      }),
+  };
+});
+
+export function updateStoreStateFromController(pageName, newState) {
+  useStore.getState().setPage(pageName, newState);
+}
+
+const desktopModeProps = {
+  ...{
+  "Home": {
+    "Flex5": {
+      "callbacks": {}
+    },
+    "Flex1": {
+      "callbacks": {}
+    },
+    "Flex2": {
+      "callbacks": {}
+    },
+    "Flex3": {
+      "callbacks": {}
+    },
+    "Flex4": {
+      "callbacks": {}
+    },
+    "Flex7": {
+      "callbacks": {}
+    },
+    "Flex8": {
+      "callbacks": {}
+    },
+    "Flex9": {
+      "callbacks": {}
+    },
+    "Flex11": {
+      "callbacks": {}
+    },
+    "Porfolio_Creater": {
+      "custom": {
+        "text": "Portfolio Creater"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "TextBox5": {
+      "custom": {
+        "text": "."
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "TextBox3": {
+      "custom": {
+        "text": "About"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "TextBox4": {
+      "custom": {
+        "text": "Blog"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "TextBox6": {
+      "custom": {
+        "text": "Services"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "TextBox7": {
+      "custom": {
+        "text": "Projects"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "TextBox8": {
+      "custom": {
+        "text": "Book a call"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "Image3": {
+      "custom": {
+        "alt": "No preview available",
+        "src": "/app-assets/right_arrow.svg"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "Flex10": {
+      "callbacks": {}
+    },
+    "TextBox11": {
+      "custom": {
+        "text": "I design products "
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "TextBox12": {
+      "custom": {
+        "text": "that delight and inspire people"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "TextBox13": {
+      "custom": {
+        "text": "Hi! I’m Jake, a product designer based in Berlin. I create user-friendly interfaces for fast-growing startups."
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "Button1": {
+      "custom": {
+        "text": "Submit"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "TextBox14": {
+      "custom": {
+        "text": "Download CV"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    },
+    "Image5": {
+      "custom": {
+        "alt": "No preview available",
+        "src": "/app-assets/right_arrow.svg"
+      },
+      "callbacks": {
+        "onClick": [
+          {
+            "sendEventData": true
+          }
+        ]
+      }
+    }
+  }
+}};
+
+useStore.setState(desktopModeProps);
+
+export default useStore;
---
+++ atri_app/app/src/pages/Home.jsx
@@ -0,0 +1,139 @@
+import { useLayoutEffect, useEffect } from "react";
+import useStore, { updateStoreStateFromController } from "../hooks/useStore";
+import useIoStore from "../hooks/useIoStore";
+import { useNavigate, useLocation } from "react-router-dom";
+import { subscribeInternalNavigation } from "../utils/navigate";
+import {fetchPageProps} from "../utils/fetchPageProps"
+import { Flex } from "@atrilabs/react-component-manifests/src/manifests/Flex/Flex.tsx";
+import { TextBox } from "@atrilabs/react-component-manifests/src/manifests/TextBox/TextBox.tsx";
+import { Image } from "@atrilabs/react-component-manifests/src/manifests/Image/Image.tsx";
+import { Button } from "@atrilabs/react-component-manifests/src/manifests/Button/Button.tsx";
+import { useFlex5Cb, useFlex1Cb, useFlex2Cb, useFlex3Cb, useFlex4Cb, useFlex7Cb, useFlex8Cb, useFlex9Cb, useFlex11Cb, usePorfolio_CreaterCb, useTextBox5Cb, useTextBox3Cb, useTextBox4Cb, useTextBox6Cb, useTextBox7Cb, useTextBox8Cb, useImage3Cb, useFlex10Cb, useTextBox11Cb, useTextBox12Cb, useTextBox13Cb, useButton1Cb, useTextBox14Cb, useImage5Cb } from "../page-cbs/Home";
+import "../page-css/Home.css";
+import "../custom/Home";
+
+export default function Home() {
+  const navigate = useNavigate();
+  useEffect(() => {
+    const unsub = subscribeInternalNavigation((url) => {
+      navigate(url);
+    });
+    return unsub;
+  }, [navigate]);
+
+  const location = useLocation();
+  useLayoutEffect(()=>{
+    fetchPageProps(location.pathname, location.search).then((res)=>{
+      updateStoreStateFromController(res.pageName, res.pageState)
+    })
+  }, [location])
+
+  const Flex5Props = useStore((state)=>state["Home"]["Flex5"]);
+const Flex5IoProps = useIoStore((state)=>state["Home"]["Flex5"]);
+const Flex5Cb = useFlex5Cb()
+const Flex1Props = useStore((state)=>state["Home"]["Flex1"]);
+const Flex1IoProps = useIoStore((state)=>state["Home"]["Flex1"]);
+const Flex1Cb = useFlex1Cb()
+const Flex2Props = useStore((state)=>state["Home"]["Flex2"]);
+const Flex2IoProps = useIoStore((state)=>state["Home"]["Flex2"]);
+const Flex2Cb = useFlex2Cb()
+const Flex3Props = useStore((state)=>state["Home"]["Flex3"]);
+const Flex3IoProps = useIoStore((state)=>state["Home"]["Flex3"]);
+const Flex3Cb = useFlex3Cb()
+const Flex4Props = useStore((state)=>state["Home"]["Flex4"]);
+const Flex4IoProps = useIoStore((state)=>state["Home"]["Flex4"]);
+const Flex4Cb = useFlex4Cb()
+const Flex7Props = useStore((state)=>state["Home"]["Flex7"]);
+const Flex7IoProps = useIoStore((state)=>state["Home"]["Flex7"]);
+const Flex7Cb = useFlex7Cb()
+const Flex8Props = useStore((state)=>state["Home"]["Flex8"]);
+const Flex8IoProps = useIoStore((state)=>state["Home"]["Flex8"]);
+const Flex8Cb = useFlex8Cb()
+const Flex9Props = useStore((state)=>state["Home"]["Flex9"]);
+const Flex9IoProps = useIoStore((state)=>state["Home"]["Flex9"]);
+const Flex9Cb = useFlex9Cb()
+const Flex11Props = useStore((state)=>state["Home"]["Flex11"]);
+const Flex11IoProps = useIoStore((state)=>state["Home"]["Flex11"]);
+const Flex11Cb = useFlex11Cb()
+const Porfolio_CreaterProps = useStore((state)=>state["Home"]["Porfolio_Creater"]);
+const Porfolio_CreaterIoProps = useIoStore((state)=>state["Home"]["Porfolio_Creater"]);
+const Porfolio_CreaterCb = usePorfolio_CreaterCb()
+const TextBox5Props = useStore((state)=>state["Home"]["TextBox5"]);
+const TextBox5IoProps = useIoStore((state)=>state["Home"]["TextBox5"]);
+const TextBox5Cb = useTextBox5Cb()
+const TextBox3Props = useStore((state)=>state["Home"]["TextBox3"]);
+const TextBox3IoProps = useIoStore((state)=>state["Home"]["TextBox3"]);
+const TextBox3Cb = useTextBox3Cb()
+const TextBox4Props = useStore((state)=>state["Home"]["TextBox4"]);
+const TextBox4IoProps = useIoStore((state)=>state["Home"]["TextBox4"]);
+const TextBox4Cb = useTextBox4Cb()
+const TextBox6Props = useStore((state)=>state["Home"]["TextBox6"]);
+const TextBox6IoProps = useIoStore((state)=>state["Home"]["TextBox6"]);
+const TextBox6Cb = useTextBox6Cb()
+const TextBox7Props = useStore((state)=>state["Home"]["TextBox7"]);
+const TextBox7IoProps = useIoStore((state)=>state["Home"]["TextBox7"]);
+const TextBox7Cb = useTextBox7Cb()
+const TextBox8Props = useStore((state)=>state["Home"]["TextBox8"]);
+const TextBox8IoProps = useIoStore((state)=>state["Home"]["TextBox8"]);
+const TextBox8Cb = useTextBox8Cb()
+const Image3Props = useStore((state)=>state["Home"]["Image3"]);
+const Image3IoProps = useIoStore((state)=>state["Home"]["Image3"]);
+const Image3Cb = useImage3Cb()
+const Flex10Props = useStore((state)=>state["Home"]["Flex10"]);
+const Flex10IoProps = useIoStore((state)=>state["Home"]["Flex10"]);
+const Flex10Cb = useFlex10Cb()
+const TextBox11Props = useStore((state)=>state["Home"]["TextBox11"]);
+const TextBox11IoProps = useIoStore((state)=>state["Home"]["TextBox11"]);
+const TextBox11Cb = useTextBox11Cb()
+const TextBox12Props = useStore((state)=>state["Home"]["TextBox12"]);
+const TextBox12IoProps = useIoStore((state)=>state["Home"]["TextBox12"]);
+const TextBox12Cb = useTextBox12Cb()
+const TextBox13Props = useStore((state)=>state["Home"]["TextBox13"]);
+const TextBox13IoProps = useIoStore((state)=>state["Home"]["TextBox13"]);
+const TextBox13Cb = useTextBox13Cb()
+const Button1Props = useStore((state)=>state["Home"]["Button1"]);
+const Button1IoProps = useIoStore((state)=>state["Home"]["Button1"]);
+const Button1Cb = useButton1Cb()
+const TextBox14Props = useStore((state)=>state["Home"]["TextBox14"]);
+const TextBox14IoProps = useIoStore((state)=>state["Home"]["TextBox14"]);
+const TextBox14Cb = useTextBox14Cb()
+const Image5Props = useStore((state)=>state["Home"]["Image5"]);
+const Image5IoProps = useIoStore((state)=>state["Home"]["Image5"]);
+const Image5Cb = useImage5Cb()
+
+  return (<>
+  <Flex className="p-Home Flex5 bpt" {...Flex5Props} {...Flex5Cb} {...Flex5IoProps}>
+<Flex className="p-Home Flex1 bpt" {...Flex1Props} {...Flex1Cb} {...Flex1IoProps}>
+<Flex className="p-Home Flex2 bpt" {...Flex2Props} {...Flex2Cb} {...Flex2IoProps}>
+<TextBox className="p-Home Porfolio_Creater bpt" {...Porfolio_CreaterProps} {...Porfolio_CreaterCb} {...Porfolio_CreaterIoProps}/>
+<TextBox className="p-Home TextBox5 bpt" {...TextBox5Props} {...TextBox5Cb} {...TextBox5IoProps}/>
+</Flex>
+<Flex className="p-Home Flex3 bpt" {...Flex3Props} {...Flex3Cb} {...Flex3IoProps}>
+<TextBox className="p-Home TextBox3 bpt" {...TextBox3Props} {...TextBox3Cb} {...TextBox3IoProps}/>
+<TextBox className="p-Home TextBox6 bpt" {...TextBox6Props} {...TextBox6Cb} {...TextBox6IoProps}/>
+<TextBox className="p-Home TextBox7 bpt" {...TextBox7Props} {...TextBox7Cb} {...TextBox7IoProps}/>
+<TextBox className="p-Home TextBox4 bpt" {...TextBox4Props} {...TextBox4Cb} {...TextBox4IoProps}/>
+<Flex className="p-Home Flex4 bpt" {...Flex4Props} {...Flex4Cb} {...Flex4IoProps}>
+<TextBox className="p-Home TextBox8 bpt" {...TextBox8Props} {...TextBox8Cb} {...TextBox8IoProps}/>
+<Image className="p-Home Image3 bpt" {...Image3Props} {...Image3Cb} {...Image3IoProps}/>
+</Flex>
+</Flex>
+</Flex>
+</Flex>
+<Flex className="p-Home Flex7 bpt" {...Flex7Props} {...Flex7Cb} {...Flex7IoProps}>
+<Flex className="p-Home Flex8 bpt" {...Flex8Props} {...Flex8Cb} {...Flex8IoProps}>
+<Flex className="p-Home Flex9 bpt" {...Flex9Props} {...Flex9Cb} {...Flex9IoProps}>
+<TextBox className="p-Home TextBox11 bpt" {...TextBox11Props} {...TextBox11Cb} {...TextBox11IoProps}/>
+<TextBox className="p-Home TextBox12 bpt" {...TextBox12Props} {...TextBox12Cb} {...TextBox12IoProps}/>
+<TextBox className="p-Home TextBox13 bpt" {...TextBox13Props} {...TextBox13Cb} {...TextBox13IoProps}/>
+<Flex className="p-Home Flex11 bpt" {...Flex11Props} {...Flex11Cb} {...Flex11IoProps}>
+<Button className="p-Home Button1 bpt" {...Button1Props} {...Button1Cb} {...Button1IoProps}/>
+<TextBox className="p-Home TextBox14 bpt" {...TextBox14Props} {...TextBox14Cb} {...TextBox14IoProps}/>
+<Image className="p-Home Image5 bpt" {...Image5Props} {...Image5Cb} {...Image5IoProps}/>
+</Flex>
+</Flex>
+<Flex className="p-Home Flex10 bpt" {...Flex10Props} {...Flex10Cb} {...Flex10IoProps}/>
+</Flex>
+</Flex>
+  </>);
+}
